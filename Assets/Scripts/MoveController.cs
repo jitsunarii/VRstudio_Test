c1fd49dc4887503d4d2be855c9139b9280599a4f
jitsunarii <jitsunari931027@gmail.com>
2024-08-03 20:03:28 +0900

    Has Exit Timeを無効にしたら動作がスムーズになった

--- Assets/Scripts/MoveController.cs
+++ Assets/Scripts/MoveController.cs
@@ -1,34 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public class MoveController : MonoBehaviour
 
+public class MoveController : MonoBehaviour
 {
     private Animator animator;
+
     // Start is called before the first frame update
-    void Start()  {
+    void Start()
+    {
         animator = GetComponent<Animator>();
     }
-    // Update is called once per frame
-    void Update()  {
-        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))   {
-            animator.SetBool("Walk", true);
-            transform.position += transform.forward * 0.01f;
-        } else {
-            animator.SetBool("Walk", false);
-        }
 
-        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))  {
+    // Update is called once per frame
+    void Update()
+    {
+        bool isWalking = Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W);
+        bool isBacking = Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S);
+
+        // Walk
+        if (isWalking)
+        {
+            animator.SetBool("Walk", true);
+            animator.SetBool("Back", false);
+            transform.position += transform.forward * 0.01f;
+        }
+        // Back
+        else if (isBacking)
+        {
+            animator.SetBool("Walk", false);
             animator.SetBool("Back", true);
             transform.position -= transform.forward * 0.01f;
-        }  else  {
+        }
+        // State
+        else
+        {
+            animator.SetBool("Walk", false);
             animator.SetBool("Back", false);
         }
 
-        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))   {
+        // Rotation
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
             transform.Rotate(0.0f, 1.0f, 0.0f);
         }
-        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))   {
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
             transform.Rotate(0.0f, -1.0f, 0.0f);
         }
     }
